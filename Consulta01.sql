52a56b80a9dd399c93461ddfa56faa8f1272cce3
Israel Moura <127752331+raelzim@users.noreply.github.com>
2023-05-25 11:43:08 -0300

    Add files via upload

---
+++ Consulta01.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE bancoa3;
+
+USE bancoa3;
+
+CREATE TABLE login (
+	nome VARCHAR(300),
+	login VARCHAR (100),
+	senha VARCHAR (100),
+	PRIMARY KEY (login)
+);
+INSERT INTO login (nome, login, senha)
+VALUES ('Administrador1','admin','fpb123');
